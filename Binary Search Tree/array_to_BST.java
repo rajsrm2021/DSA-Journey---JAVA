f0f2c65a5fd8acc209990850dd1643912e5760fb
rajsrm2021 <rj9769@srmist.edu.in>
2023-08-29 22:16:21 +0530

    array to BST

---
+++ Binary Search Tree/array_to_BST.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode toBST(int[] nums, int low, int high){
+        
+        if(low > high) return null;
+        int mid = (low+high) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+
+        root.left = toBST(nums, low, mid-1);
+        root.right = toBST(nums, mid+1, high);
+
+        return root;
+        
+    }
+    public TreeNode sortedArrayToBST(int[] nums) {
+
+        if(nums.length == 0 || nums == null) return null;
+
+        return toBST(nums, 0, nums.length-1);    
+        
+    }
+}
