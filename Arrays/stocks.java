73d66b85cdc24726f57db797fe80918d667a7753
rajsrm2021 <rj9769@srmist.edu.in>
2023-09-06 22:27:14 +0530

    best time to buy and sell stock

--- Arrays/stocks.java
+++ Arrays/stocks.java
@@ -3,6 +3,8 @@
 class stocks {
     public static int maxProfit(int[] prices) {
 
+        if(prices.length==0) return 0;
+
         int maxprofit = 0;
         int minSoFar = prices[0];
 
@@ -17,8 +19,10 @@ class stocks {
 
     public static void main(String args[]){
         int price[] = {7,1,5,3,6,4};
+        int price2[] = {};
 
         System.out.println(maxProfit(price));
+        System.out.println(maxProfit(price2));
 
     }
 }
