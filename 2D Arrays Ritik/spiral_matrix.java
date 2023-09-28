c91be5635c62f0ee266d0c5551533b6f8f47e3da
rp4563 <rp4563@srmist.edu.in>
2023-09-28 15:34:01 +0530

    staircase searching added

--- 2D Arrays Ritik/spiral_matrix.java
+++ 2D Arrays Ritik/spiral_matrix.java
@@ -42,8 +42,8 @@ public class Main
 		int matrix[][]={{1,2,3},
 		                {4,5,6},
 		                {7,8,9}};
-		               
-		
+		                                
+		                                              
 		printSpiral(matrix);
 	}
 }
