51a537f289ca2ccf35446ad00a16f8e973f69b02
rajsrm2021 <rj9769@srmist.edu.in>
2023-10-10 23:21:44 +0530

    removeDuplicates

---
+++ 150 Interview Sheet LeetCode/four.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        int uniqueCount = 0;
+        
+        for (int num : nums) {
+          
+            if (uniqueCount < 2 || num != nums[uniqueCount - 2]) {
+                nums[uniqueCount] = num;
+                uniqueCount++;
+            }
+        }
+        
+        return uniqueCount;
+    }
+}
