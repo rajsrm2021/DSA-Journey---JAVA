0cf3210fdb208a57859e7778e44b8ac395f6d8de
rajsrm2021 <rj9769@srmist.edu.in>
2023-10-16 01:30:50 +0530

    insert at middle

--- Arrays Practice/insertion.java
+++ Arrays Practice/insertion.java
@@ -1,6 +1,32 @@
+import java.util.*;
+
 public class insertion {
-    public static void main(String args[]){
-        System.out.println("divju");
+    public static void insert(int arr[],int idx, int val) {
+
+        int arr2[] = new int[arr.length + 1];
+        
+        for(int i=0;i<idx;i++){
+            arr2[i]=arr[i];
+        }
+        arr2[idx] = val;
+
+        for (int i = idx+1; i <= arr.length; i++) {
+            arr2[i]=arr[i-1];
+            
+        }
+
+
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.print(arr2[i] + " ");
+        }
     }
-    
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int arr[] = { 1, 2, 3};
+        insert(arr,1,0);
+        System.out.println();
+        System.out.println(arr.length);
+
+    }
 }
