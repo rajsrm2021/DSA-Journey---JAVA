e216061a77f9e5d26ee083b17b6eb0e2f2df577d
rajsrm2021 <rj9769@srmist.edu.in>
2023-10-16 02:12:25 +0530

    init

--- Arrays Practice/insertion.java
+++ Arrays Practice/insertion.java
@@ -1,18 +1,18 @@
 import java.util.*;
 
 public class insertion {
-    public static void insert(int arr[],int idx, int val) {
+    public static void insert(int arr[], int idx, int val) {
 
         int arr2[] = new int[arr.length + 1];
-        
-        for(int i=0;i<idx;i++){
-            arr2[i]=arr[i];
+
+        for (int i = 0; i < idx; i++) {
+            arr2[i] = arr[i];
         }
         arr2[idx] = val;
 
-        for (int i = idx+1; i <= arr.length; i++) {
-            arr2[i]=arr[i-1];
-            
+        for (int i = idx + 1; i <= arr.length; i++) {
+            arr2[i] = arr[i - 1];
+
         }
 
 
@@ -21,12 +21,29 @@ public class insertion {
         }
     }
 
+    public static void remove(int[] arr,int idx){
+
+        int arr2[] = new int[arr.length-1];
+
+        for (int i = 0; i < idx-1; i++) {
+            arr2[i] = arr[i];
+        }
+
+        for(int i=idx;i<arr.length;i++){
+            arr2[i-1] = arr[i];
+        }
+
+        for (int i : arr2) {
+            System.out.print(i+" ");
+        }
+    }
+
+
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        int arr[] = { 1, 2, 3};
-        insert(arr,1,0);
-        System.out.println();
-        System.out.println(arr.length);
+        int arr[] = { 1, 2, 3, 4 };
+
+        remove(arr,3);
 
     }
 }
