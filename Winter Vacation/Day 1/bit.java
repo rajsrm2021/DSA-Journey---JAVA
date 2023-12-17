53d4f58ead24e63e4c038cab27bd3f29114dc695
rajsrm2021 <rj9769@srmist.edu.in>
2023-12-17 20:49:21 +0545

    bit manupulation

--- Winter Vacation/Day 1/bit.java
+++ Winter Vacation/Day 1/bit.java
@@ -2,6 +2,16 @@ import java.util.*;
 
 public class bit {
     public static void main(String args[]) {
-        System.out.println("hello ji");
+        int a = 11;
+        int b = a >> 1; // right shift
+        int c = a << 1; // left sift
+        System.out.println(b);
+        System.out.println(c);
+
+        if ((a & 1) == 0) { // bit masking concept
+            System.out.println("even");
+        } else {
+            System.out.println("odd");
+        }
     }
 }
