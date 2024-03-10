ed26df76cf5d62c1feef4fbe13564a854cd57131
rajsrm2021 <rj9769@srmist.edu.in>
2024-03-11 00:09:55 +0530

    sorting algo

--- Sorting algorithms/bubble_sort.java
+++ Sorting algorithms/bubble_sort.java
@@ -1,5 +1,36 @@
-package Sorting algorithms;
 
 public class bubble_sort {
-    
+
+    public static void sort(int arr[]) {
+
+        for (int j = 0; j < arr.length - 1; j++) {
+            for (int i = 0; i < arr.length - j - 1; i++) {
+
+                if (arr[i] > arr[i + 1]) {
+                    // swap using xor
+                    arr[i] ^= arr[i + 1];
+                    arr[i + 1] ^= arr[i];
+                    arr[i] ^= arr[i + 1];
+
+                }
+
+            }
+        }
+    }
+
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String args[]) {
+
+        int arr[] = { 1, 2, 4, 55, 2, 66, 3, 1, 6 };
+
+        sort(arr);
+
+        printArr(arr);
+
+    }
 }
