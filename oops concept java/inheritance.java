e1d6bb9417f0e2369cc4abfe30de5e54b698af80
rajsrm2021 <rj9769@srmist.edu.in>
2024-05-17 11:09:59 +0530

    oops

--- oops concept java/inheritance.java
+++ oops concept java/inheritance.java
@@ -1,5 +1,33 @@
-package oops concept java;
-
 public class inheritance {
-    
+
+    static class Animal {
+        public void behaviour() {
+            System.out.println("Different animals have different sounds");
+        }
+    }
+
+    static class Dog extends Animal {
+        @Override
+        public void behaviour() {
+            System.out.println("Dog barks");
+        }
+    }
+
+    static class Cat extends Animal {
+        @Override
+        public void behaviour() {
+            System.out.println("Cat meows");
+        }
+    }
+
+    public static void main(String[] args) {
+        Animal myAnimal = new Animal();
+        myAnimal.behaviour(); // Outputs: Different animals have different sounds
+
+        Dog myDog = new Dog();
+        myDog.behaviour(); // Outputs: Dog barks
+
+        Cat myCat = new Cat();
+        myCat.behaviour(); // Outputs: Cat meows
+    }
 }
