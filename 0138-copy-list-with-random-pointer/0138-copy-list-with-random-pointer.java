ed91da2b51f3431f342347794f256434070f26dd
Raj Kumar Jaiswal <rj9769@srmist.edu.in>
2024-06-23 12:14:36 +0530

    Time: 0 ms (100%), Space: 44.2 MB (59.8%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -15,40 +15,48 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
-        if (head == null) return null;
 
-        // Step 1: Insert nodes in between
+        if(head == null) return null;
+
+        // step1: bich me ghusana
         Node curr = head;
-        while (curr != null) {
+
+        while(curr!=null){
             Node newNode = new Node(curr.val);
             newNode.next = curr.next;
             curr.next = newNode;
             curr = newNode.next;
         }
 
-        // Step 2: Assign random pointers for the new nodes
+        // step2 : random variable
+
         curr = head;
-        while (curr != null) {
-            if (curr.random != null) {
+        while(curr!=null){
+            if(curr.random!=null){
                 curr.next.random = curr.random.next;
             }
+
             curr = curr.next.next;
         }
 
-        // Step 3: Separate the original and copied lists
+        // step 3: breal the connextions
+
         curr = head;
-        Node newHead = head.next;
+        Node newHead = curr.next;
         Node newCurr = newHead;
 
-        while (curr != null) {
+        while(curr !=null){
             curr.next = newCurr.next;
             curr = curr.next;
-            if (curr != null) {
+
+            if(curr!=null){
                 newCurr.next = curr.next;
                 newCurr = newCurr.next;
             }
         }
 
         return newHead;
+        
+        
     }
 }
