37e8a8027ec5ab0adcee3fc81186e1d98c368685
Raj Kumar Jaiswal <rj9769@srmist.edu.in>
2024-06-30 12:11:50 +0530

    Time: 16 ms (9.13%), Space: 47.5 MB (13.73%) - LeetHub

---
+++ 0498-diagonal-traverse/0498-diagonal-traverse.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] findDiagonalOrder(int[][] mat) {
+
+        int n = mat.length;
+        int m = mat[0].length;
+
+        Map<Integer,List<Integer>> map = new HashMap<>();
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+               int key = i+j;
+               List<Integer> list = map.getOrDefault(key,new ArrayList<>());
+               list.add(mat[i][j]);
+               map.put(key,list);
+            }
+        }
+
+        List<Integer> res = new ArrayList<>();
+        for(int i=0;i<m+n-1;i++){
+            List<Integer> temp = map.get(i);
+            if(i%2==0){
+                Collections.reverse(temp);
+            }
+
+            res.addAll(temp);
+        }
+        return res.stream().mapToInt(i -> i).toArray();
+        
+    }
+}
