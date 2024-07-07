ac1c5aea2f6c757c91d9539a401f29403f5bd803
Raj Kumar Jaiswal <rj9769@srmist.edu.in>
2024-07-07 15:53:59 +0530

    Time: 25 ms (100%), Space: 56.1 MB (100%) - LeetHub

--- 3264-maximum-points-after-enemy-battles/3264-maximum-points-after-enemy-battles.java
+++ 3264-maximum-points-after-enemy-battles/3264-maximum-points-after-enemy-battles.java
@@ -1,17 +1,23 @@
 class Solution {
     public long maximumPoints(int[] enemyEnergies, int currentEnergy) {
-
+        long profit = 0;
+        int n = enemyEnergies.length;
         Arrays.sort(enemyEnergies);
         
-        if(currentEnergy < enemyEnergies[0]) return 0;
-
-        long totalEnergy = currentEnergy;
-
-        for(int i=enemyEnergies.length-1; i>0;i--){
-            totalEnergy += enemyEnergies[i];
-        }
-
-        return totalEnergy/enemyEnergies[0];
+        if (enemyEnergies[0] > currentEnergy)
+            return 0;
         
+        int j = n - 1;
+        while (j >= 0) {
+            if (enemyEnergies[0] <= currentEnergy) {
+                profit += currentEnergy / enemyEnergies[0];
+                currentEnergy %= enemyEnergies[0];
+            } else {
+                currentEnergy += enemyEnergies[j];
+                j--;
+            }
+        }
+        
+        return profit;
     }
 }
