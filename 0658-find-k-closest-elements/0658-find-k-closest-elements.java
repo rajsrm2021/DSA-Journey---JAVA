0837868e19e9920536399d688d978c02d0368adf
Raj Kumar Jaiswal <rj9769@srmist.edu.in>
2024-07-07 14:03:19 +0530

    Time: 43 ms (15.24%), Space: 46.5 MB (6.21%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,38 +1,41 @@
 class Solution {
+
     public class Pair implements Comparable<Pair>{
         int diff;
         int val;
 
-        Pair(int diff, int val) {
+        Pair(int diff, int val){
             this.diff = diff;
             this.val = val;
         }
 
-        public int compareTo(Pair p2){
-            return this.diff==p2.diff?p2.val-this.val : p2.diff-this.diff;
+      
+       public int compareTo(Pair p2){
+    return this.diff==p2.diff ? p2.val-this.val : p2.diff-this.diff;
 }
-    }
 
+    }
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
+
         PriorityQueue<Pair> pq = new PriorityQueue<>();
 
         for(int i=0;i<arr.length;i++){
-            pq.offer(new Pair(Math.abs(x-arr[i]),i));
+            pq.offer(new Pair(Math.abs(x-arr[i]),arr[i]));
 
             if(pq.size()>k){
                 pq.poll();
             }
         }
 
-        List<Integer> res = new ArrayList<>();
+        List<Integer> l = new ArrayList<>();
 
         while(!pq.isEmpty()){
-            res.add(arr[pq.poll().val]);
+            l.add(pq.poll().val);
         }
 
-        Collections.sort(res);
+        Collections.sort(l);
 
-        return res;
-
+        return l;
+        
     }
 }
