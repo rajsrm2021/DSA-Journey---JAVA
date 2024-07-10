589c881ce6e06b4b1fdc65c3565037f8fc110046
Raj Kumar Jaiswal <rj9769@srmist.edu.in>
2024-07-11 00:42:12 +0530

    Time: 1 ms (99.42%), Space: 42.2 MB (19.68%) - LeetHub

--- 1720-crawler-log-folder/1720-crawler-log-folder.java
+++ 1720-crawler-log-folder/1720-crawler-log-folder.java
@@ -1,21 +1,22 @@
 class Solution {
     public int minOperations(String[] logs) {
 
-        int count =0;
+        int count = 0;
 
-        for(int i=0;i<logs.length;i++){
-            
-            if(logs[i].equals("../")){
+        for (int i = 0; i < logs.length; i++) {
+
+            if (logs[i].equals("../") ) {
                 count--;
-                if(count<0) {
-                    count =0;
+                if(count<0){
+                    count=0;
                 }
-            }else if(logs[i].equals("./")){
+
+            } else if (logs[i].equals("./")) {
                 continue;
-            }else{
-                count ++;
+            } else {
+                count++;
             }
         }
-       return count;
+        return count;
     }
 }
