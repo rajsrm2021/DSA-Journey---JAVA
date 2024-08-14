45c3789c806856e0f52f3ddc3697a7c6171b12e1
Raj Kumar Jaiswal <rj9769@srmist.edu.in>
2024-08-14 13:51:44 +0530

    Time: 0 ms (100%), Space: 44.7 MB (41.06%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -3,23 +3,15 @@ class Solution {
 
         int count = 0;
 
-        if(nums.length==1){
-            return 1;
-        }
-
-        for(int i=0;i<nums.length-1;i++){
-            if(nums[i]!=nums[i+1]){
-                nums[count] = nums[i];
+        for(int i=1;i<nums.length;i++){
+            if(nums[count]!=nums[i]){
                 count++;
+                nums[count] = nums[i];
+                
             }
         }
 
-        if(nums[nums.length-1]!=nums[count]){
-            nums[count] = nums[nums.length-1];
-            count++;
-        }
-
-        return count;
+        return count+1;
         
     }
 }
