1e201f260d855e4a32553f91b18e70fc1d2d95e8
Raj Kumar Jaiswal <rj9769@srmist.edu.in>
2024-08-15 11:36:41 +0530

    Time: 1 ms (99.98%), Space: 45.6 MB (53.39%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,17 +1,16 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int n = nums.length;
-        int goal = n - 1;  // Start with the last index as the goal
-        
-        // Traverse the array from the end to the beginning
-        for (int i = n - 1; i >= 0; i--) {
-            // If from index i, you can reach or go beyond the current goal, update the goal to i
-            if (i + nums[i] >= goal) {
+
+        int n = nums.length - 1;
+        int goal = n;
+
+        for (int i = n; i >= 0; i--) {
+            if(nums[i] + i >= goal){
                 goal = i;
             }
         }
-        
-        // If the goal has reached the start of the array, return true
-        return goal == 0;
+
+        return goal ==0;
+
     }
 }
