536c19d2721c79a119176cd025dcf32a8de85403
Raj Kumar Jaiswal <rj9769@srmist.edu.in>
2024-08-20 00:15:21 +0530

    Time: 20 ms (25.46%), Space: 55.6 MB (9.38%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.java
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.java
@@ -1,22 +1,34 @@
 class Solution {
-    int solve(int curr, int clipA, int n){
-        if(curr==n){
+    int solve(int curr, int clipA, int n, int memo[][]) {
+        if (curr == n) {
             return 0;
         }
-        if(curr > n) {
+        if (curr > n) {
             return 10000;
         }
 
-        int copyAllPaste = 1 + 1 + solve(curr + curr, curr, n);
+        if (memo[curr][clipA] != -1) {
+            return memo[curr][clipA];
+        }
 
-        int past = 1 + solve(curr + clipA , clipA, n);
+        int copyAllPaste = 1 + 1 + solve(curr + curr, curr, n,memo);
 
-        return Math.min(copyAllPaste,past);
+        int past = 1 + solve(curr + clipA, clipA, n,memo);
+
+        return memo[curr][clipA] = Math.min(copyAllPaste, past);
     }
-    public int minSteps(int n) {
-        if(n==1) return 0;
 
-        return 1 + solve(1,1, n);
-        
+    public int minSteps(int n) {
+        if (n == 1)
+            return 0;
+
+        int memo[][] = new int[n + 1][n + 1];
+
+        for (int[] row : memo) {
+            Arrays.fill(row, -1);
+        }
+
+        return 1 + solve(1, 1, n, memo);
+
     }
 }
