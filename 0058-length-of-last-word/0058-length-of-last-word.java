0188697988442127dad3df0599ac99a5f2b4be1a
Raj Kumar Jaiswal <rj9769@srmist.edu.in>
2024-08-20 19:27:39 +0530

    Time: 0 ms (100%), Space: 41.9 MB (18.6%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -7,13 +7,11 @@ class Solution {
 
         int count = 0;
 
-        for (int i = s.length() - 1; i >= 0; i--) 
+        for (int i = s.length() - 1; i >= 0; i--)
             if (s.charAt(i) != ' ')
                 count++;
             else if (count > 0)
                 return count;
-
-        
 
         return count;
 
