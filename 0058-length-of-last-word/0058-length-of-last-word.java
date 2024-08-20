977e091a74c0b08110622d877735d40dc2022bc8
Raj Kumar Jaiswal <rj9769@srmist.edu.in>
2024-08-20 19:25:42 +0530

    Time: 0 ms (100%), Space: 41.8 MB (30.46%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,9 +1,23 @@
 class Solution {
     public int lengthOfLastWord(String s) {
 
-        String[] str = s.split(" ");
+        // edge case
+        if (s.length() == 0)
+            return 0;
 
-        return str[str.length-1].length();
-        
+        int count = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') {
+                count++;
+            } else {
+                if (count > 0) {
+                    return count;
+                }
+            }
+        }
+
+        return count;
+
     }
 }
