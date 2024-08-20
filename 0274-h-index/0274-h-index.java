4d4ab2806840f201b2c07630897235dd4deaf733
Raj Kumar Jaiswal <rj9769@srmist.edu.in>
2024-08-20 12:43:58 +0530

    Time: 2 ms (71.33%), Space: 41.4 MB (65.76%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,19 +1,30 @@
-class Solution {
+import java.util.Arrays;
+
+public class Solution {
     public int hIndex(int[] citations) {
-
+        Arrays.sort(citations); // Sort the array in ascending order
+        
         int n = citations.length;
+        int left = 0;
+        int right = n - 1;
 
-        Arrays.sort(citations);
-
-        int hindex = 0;
-
-        for(int i = n-1;i>=0;i--){
-            if(citations[i] >= n-i){
-                hindex = n - i;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int hIndex = n - mid; // Number of papers with at least hIndex citations
+            
+            if (citations[mid] >= hIndex) {
+                right = mid - 1; // Check if there is a higher possible H-index
+            } else {
+                left = mid + 1; // Increase the search range
             }
         }
 
-        return hindex;
-        
+        return n - left; // Return the highest possible H-index found
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] citations = {6, 5, 3, 1, 0}; // Example array
+        System.out.println("H-index: " + sol.hIndex(citations)); // Output the H-index
     }
 }
