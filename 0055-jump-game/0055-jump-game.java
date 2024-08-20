0f3a83395aa5a6b4dca0db4371c8806151d52778
Raj Kumar Jaiswal <rj9769@srmist.edu.in>
2024-08-20 11:39:30 +0530

    Time: 1 ms (99.98%), Space: 45.5 MB (54.12%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -5,7 +5,8 @@ class Solution {
         int goal = n;
 
         for (int i = n; i >= 0; i--) {
-            if(nums[i] + i >= goal){
+     // nums i par hum bani or ooijan se hum i se kana aage jayem ooise i add kara tani hum 
+            if(nums[i] + i >= goal){  
                 goal = i;
             }
         }
