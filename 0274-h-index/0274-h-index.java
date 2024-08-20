5bf49a365a64d900469e30ffe62fa469cc942be1
Raj Kumar Jaiswal <rj9769@srmist.edu.in>
2024-08-20 12:36:41 +0530

    Time: 2 ms (71.33%), Space: 41.7 MB (27.34%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,24 +1,19 @@
 class Solution {
     public int hIndex(int[] citations) {
-        // Sort the citations array
-        Arrays.sort(citations);
 
         int n = citations.length;
-        int left = 0, right = n - 1;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            int h = n - mid;
+        Arrays.sort(citations);
 
-            if (citations[mid] == h) {
-                return h; // Found the exact h-index
-            } else if (citations[mid] < h) {
-                left = mid + 1; // Move right to increase h
-            } else {
-                right = mid - 1; // Move left to decrease h
+        int hindex = 0;
+
+        for(int i = n-1;i>=0;i--){
+            if(citations[i] >= n-i){
+                hindex = n - i;
             }
         }
 
-        return n - left;
+        return hindex;
+        
     }
 }
