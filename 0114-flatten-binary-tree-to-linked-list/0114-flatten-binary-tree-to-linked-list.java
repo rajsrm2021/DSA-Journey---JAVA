57ff726d37353fc253ea8edf0d978415a8329e3d
Raj Kumar Jaiswal <rj9769@srmist.edu.in>
2024-09-11 23:45:24 +0530

    Time: 1 ms (23.34%), Space: 42 MB (69.42%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -15,20 +15,23 @@
  */
 class Solution {
     Queue<TreeNode> queue = new LinkedList<>();
-    public void addToQueue(TreeNode root){
-        if(root == null){
-            return;
-        }
-        queue.add(root);         
+
+    public  void addToQueue(TreeNode root){
+        if(root==null) return;
+
+        queue.add(root);
         addToQueue(root.left);
         addToQueue(root.right);
     }
     public void flatten(TreeNode root) {
+
         addToQueue(root);
-        while(!queue.isEmpty()){   
+
+        while(!queue.isEmpty()){
             TreeNode temp = queue.poll();
             temp.right = queue.peek();
             temp.left = null;
         }
+        
     }
 }
