e214328394e1287dae24955b946337639a4f3be6
Raj Kumar Jaiswal <rj9769@srmist.edu.in>
2024-12-14 20:05:43 +0530

    Time: 28 ms (53.03%), Space: 57.1 MB (90.14%) - LeetHub

--- 2868-continuous-subarrays/2868-continuous-subarrays.java
+++ 2868-continuous-subarrays/2868-continuous-subarrays.java
@@ -1,31 +1,30 @@
-import java.util.TreeMap;
-
 class Solution {
     public long continuousSubarrays(int[] nums) {
-        int n = nums.length;
-        long ans = 0;
+
+        TreeMap<Integer,Integer> map = new TreeMap<>();
+
         int left = 0;
 
-        TreeMap<Integer, Integer> map = new TreeMap<>();
+        long ans = 0;
 
-        for (int right = 0; right < n; right++) {
-            // Add the current number to the TreeMap
-            map.put(nums[right], map.getOrDefault(nums[right], 0) + 1);
+        for(int right = 0;right<nums.length;right++){
 
-            // Check if the difference between the smallest and largest elements exceeds 2
-            while (map.lastKey() - map.firstKey() > 2) {
+            map.put(nums[right], map.getOrDefault(nums[right],0)+1);
+
+            while(map.lastKey() - map.firstKey() > 2){
                 int leftValue = nums[left];
-                map.put(leftValue, map.get(leftValue) - 1);
-                if (map.get(leftValue) == 0) {
+                map.put(leftValue,map.get(nums[left]) -1 );
+
+                if(map.get(leftValue) == 0){
                     map.remove(leftValue);
                 }
+
                 left++;
             }
 
-            // Count all valid subarrays ending at 'right'
-            ans += (right - left + 1);
+            ans+=(right-left) + 1;
         }
-
         return ans;
+        
     }
 }
